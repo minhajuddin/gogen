655c0518ca45c6d2cecbb9df61dd2d3a0072c355
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-30 16:11:54 +0530

    Cleaned up the code a little

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	"log"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 var (
-	templates = template.Must(template.ParseGlob("./layouts/*html"))
+	templates  *template.Template
+	currentDir string
+	opDir      string
 )
 
-func main(){
+func main() {
 	log.Println("Generating site")
-	templates.ExecuteTemplate(os.Stdout, "app.html", Page{Content: "Awesome page"})
+
+	pages, _ := filepath.Glob(path.Join(currentDir, "pages", "*txt"))
+	os.Mkdir(opDir, 0700)
+
+	for _, page := range pages {
+		log.Println("Rendering", page)
+		content, _ := ioutil.ReadFile(page)
+		basename := path.Base(page)
+		filename := basename[:strings.LastIndex(basename, ".")]
+		opFile, err := os.Create(path.Join(opDir, filename+".html"))
+		if err != nil {
+			log.Panic(err)
+		}
+		defer opFile.Close()
+		templates.ExecuteTemplate(opFile, "app.html", Page{Content: string(content)})
+	}
+
 	log.Println("DONE")
 }
 
 type Page struct {
 	Content string
+}
+
+func init() {
+	templates = template.Must(template.ParseGlob(path.Join(currentDir, "layouts", "*html")))
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	opDir = path.Join(currentDir, "_site")
 }
